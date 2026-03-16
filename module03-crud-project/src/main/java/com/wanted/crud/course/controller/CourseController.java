e00955a0dbaddf178d8b7f351b760ce8717bd2ce
wanted <sjw4690@gmail.com>
2026-03-16 15:39:23 +0900

    2026_03_16

--- module03-crud-project/src/main/java/com/wanted/crud/course/controller/CourseController.java
+++ module03-crud-project/src/main/java/com/wanted/crud/course/controller/CourseController.java
@@ -1,6 +1,8 @@
 package com.wanted.crud.course.controller;
 
 import com.wanted.crud.course.model.dto.CourseDTO;
+import com.wanted.crud.course.model.dto.CourseSectionDTO;
+import com.wanted.crud.course.model.dto.SectionDTO;
 import com.wanted.crud.course.model.service.CourseService;
 
 import java.util.List;
@@ -38,5 +40,49 @@ public class CourseController {
     public List<CourseDTO> findAllCourse() {
 
         return service.findAllCourses();
+    }
+
+    /**
+     * 사용자가 입력한 데이터를 바탕으로 강좌를 삽입
+     * @param title 사용자가 입력한 강좌의 제록
+     * @param description 사용자가 입력한 강좌의 설명
+     * **/
+    public Long createCourse(String title, String description) {
+
+        /* comment. 타이틀과 설명은 논리적으로 묶여야 하는 데이터이다.
+        *   authorId 는 나중에 로그인을 한 유저 객체에서 추출해서 넣어줘야 한다.
+        * */
+        CourseDTO newCourse = new CourseDTO(null, 1L, title, description, "draft");
+        return service.saveCourse(newCourse);
+    }
+
+    public boolean deleteCourseById(long id) {
+        // 문자열로 가장 쉽게 바꾸는 방법?
+        // + ""
+        // 문자를 숫자로 가장 쉽게 바꾸는 방법?
+        // + 0
+        return service.deleteCourse(id) > 0;
+    }
+
+    public CourseDTO findCourseById(long id) {
+
+        return service.findById(id);
+
+    }
+
+    public CourseSectionDTO findJoin(long courseId) {
+        return service.findCourseWithSections(courseId);
+    }
+
+    /**
+     * 트랜젝션 테스트 전용 메소드(강의와 섹션 동시 삽입)
+     * */
+    public boolean createCourseWithDefaultSection() {
+        // 강의 객체
+        CourseDTO newCourse = new CourseDTO(null, 1L , "Java Transaction Master" , "트랜젝션을 활용한 강의 등록", "draft");
+
+        SectionDTO newSection = new SectionDTO(null, null, "Chapter 1. 트랜젝션의 이해", 1);
+
+        return service.createCourseWithDefaultSection(newCourse, newSection);
     }
 }
