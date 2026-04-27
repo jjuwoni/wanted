b3624440e15b6386f86cc576a6d62f42a9aaca31
juwon <sjw4690@gmail.com>
2026-04-27 13:24:51 +0900

    2026-04-27

---
+++ module01-rest-api/src/main/java/com/wanted/restapi/section01/response/Message.java
@@ -0,0 +1,15 @@
+package com.wanted.restapi.section01.response;
+
+import lombok.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+public class Message {
+
+    private int httpStatusCode;
+    private String message;
+
+}
